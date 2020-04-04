c9446c650149ba26bc3dc2184cf25668e8f6784d
Aronne Brivio <aronne.brivio@gmail.com>
2020-04-05 01:34:53 +0200

    brewRoutine moved to dotfiles-local

--- config/fish/local.fish
+++ config/fish/local.fish
@@ -1,5 +1,17 @@
 #!/usr/bin/env fish
 
+#
+### FUNCTIONS
+#
+# Update and cleanup system with brew
+function brewRoutine
+    brew update
+    brew upgrade
+    brew cask upgrade
+    brew cleanup
+    brew doctor
+end
+
 # Launch LoL with mic support
 function lol
     cd /Applications/GG/League\ of\ Legends.app/Contents/LoL/
