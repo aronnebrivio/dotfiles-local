e3e8f3d58b712d5b1ae16cca3df8be199a67ec18
Aronne Brivio <aronne.brivio@gmail.com>
2021-04-28 12:20:20 +0200

    start/stop DIC dev environment

--- config/fish/local.fish
+++ config/fish/local.fish
@@ -17,3 +17,25 @@ function lol
     cd /Applications/League\ of\ Legends.app/Contents/LoL/
     ./LeagueClient.app/Contents/MacOS/LeagueClient
 end
+
+# PHP
+function switchPhp
+    if test $argv[1] = 8.0
+        brew unlink php && brew link php
+    else
+        brew link php@$argv[1] --overwrite
+    end
+end
+
+# Start/stop DIC dev environment
+function startDicDevEnvironment
+    cd /Users/aronnebrivio/Progetti/MadBit
+    ./start-dic-dev-environment.sh
+    cd -
+end
+
+function stopDicDevEnvironment
+    cd /Users/aronnebrivio/Progetti/MadBit
+    ./stop-dic-dev-environment.sh
+    cd -
+end
