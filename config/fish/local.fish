1ed58b586362a8933ca85c3079a6fd90e3d0c4ab
Aronne Brivio <aronne.brivio@gmail.com>
2021-12-07 09:13:18 +0100

    envs for openssl in x86_64 brew, fixes for iterm

--- config/fish/local.fish
+++ config/fish/local.fish
@@ -1,19 +1,21 @@
 #!/usr/bin/env fish
 
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+
 fish_add_path /opt/homebrew/bin
 fish_add_path /usr/local/homebrew/bin
 
 # Pyenv
-# set -x PYENV_ROOT $HOME/.pyenv
-# fish_add_path $PYENV_ROOT/bin
+set -gx PYENV_ROOT $HOME/.pyenv
+fish_add_path $PYENV_ROOT/bin
 
-# if command -v pyenv 1>/dev/null 2>&1
-#     eval (pyenv init -)
-#     eval (pyenv virtualenv-init -)
-#     eval (pyenv init --path)
-# end
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+set -gx OPENSSL_ROOT /usr/local/homebrew/opt/openssl@1.1
+set -gx LIBRARY_PATH $OPENSSL_ROOT/lib/
+set -gx CPPFLAGS -I$OPENSSL_ROOT/include
+set -gx LDFLAGS -L$OPENSSL_ROOT/lib
+set -gx PKG_CONFIG_PATH $OPENSSL_ROOT/lib/pkgconfig
+fish_add_path $OPENSSL_ROOT/bin
 
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
@@ -30,12 +32,6 @@ function brewRoutine
     brew doctor
 end
 
-# Launch LoL with mic support
-function lol
-    cd /Applications/League\ of\ Legends.app/Contents/LoL/
-    ./LeagueClient.app/Contents/MacOS/LeagueClient
-end
-
 # PHP
 function switchPhp
     if test $argv[1] = 8.0
@@ -45,15 +41,33 @@ function switchPhp
     end
 end
 
-# Start/stop DIC dev environment
-function startDicDevEnvironment
-    cd /Users/aronnebrivio/Progetti/MadBit
-    ./start-dic-dev-environment.sh
-    cd -
+# Start/stop CA Labs dev environment
+function startLabsLocalEnvironment
+    cd /Users/aronne.brivio/Projects/CA/hagrid
+    perl -pe 's|\QRUN apt-get update -y && apt-get install -y libpq-dev gcc putty && rm -rf /var/lib/apt/lists/*|RUN apt-get update -y && apt-get install -y libpq-dev libssl-dev gcc putty && rm -rf /var/lib/apt/lists/*|g' Dockerfile
+    export ENABLE_PROFILING=True
+    export async_cloud_env_session_setup=True
+    export use_new_vcf_environment=True
+    export USE_TERRAFORM_FOR_CODE_ENVIRONMENTS=True
+    export disable_lab_bridge_connection_update_from_frontend=True
+    export disable_lab_env_setup_update_from_frontend=True
+    docker-compose up -d --build
+    perl -pe 's|\QRUN apt-get update -y && apt-get install -y libpq-dev libssl-dev gcc putty && rm -rf /var/lib/apt/lists/*|RUN apt-get update -y && apt-get install -y libpq-dev gcc putty && rm -rf /var/lib/apt/lists/*|g' Dockerfile
+
+    cd /Users/aronne.brivio/Projects/CA/doubtfire
+    perl -pe 's|\QRUN apt-get update && apt-get install -y putty wget unzip|RUN apt-get update && apt-get install -y libssl-dev putty wget unzip|g' Dockerfile
+    docker-compose up -d --build doubtfire-setup doubtfire-webapp doubtfire-celery-worker
+    perl -pe 's|\QRUN apt-get update && apt-get install -y libssl-dev putty wget unzip|RUN apt-get update && apt-get install -y putty wget unzip|g' Dockerfile
+
+    cd --
 end
 
-function stopDicDevEnvironment
-    cd /Users/aronnebrivio/Progetti/MadBit
-    ./stop-dic-dev-environment.sh
-    cd -
+function stopLabsLocalEnvironment
+    cd /Users/aronne.brivio/Projects/CA/hagrid
+    docker-compose down
+
+    cd /Users/aronne.brivio/Projects/CA/doubtfire
+    docker-compose down
+
+    cd --
 end
