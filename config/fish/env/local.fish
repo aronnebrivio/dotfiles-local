5e29f78c0ff75d98122f3c3765331000753bbf1c
Aronne Brivio <aronne.brivio@gmail.com>
2022-02-23 12:41:52 +0100

    feat(path) add asdf support

--- config/fish/env/local.fish
+++ config/fish/env/local.fish
@@ -3,3 +3,6 @@
 set -gx LDFLAGS "-L/opt/homebrew/Cellar/gettext/0.21/lib -L/opt/homebrew/Cellar/openssl@1.1/1.1.1m/lib/"
 set -gx CPPFLAGS "-I/opt/homebrew/Cellar/openssl@1.1/1.1.1m/include"
 # set -gx PATH /usr/local/homebrew/opt/libpq/bin $PATH
+
+# asdf
+source (brew --prefix asdf)/libexec/asdf.fish
