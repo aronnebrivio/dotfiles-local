84d1548cee0215e84deab89d7b2fbe7a106c4273
Aronne Brivio <aronne.brivio@gmail.com>
2022-02-24 11:03:17 +0100

    feat(pyenv): move pyenv from local to global configuration

--- config/fish/local.fish
+++ config/fish/local.fish
@@ -2,13 +2,6 @@
 
 fish_add_path /opt/homebrew/bin
 
-# Pyenv
-set -gx PYENV_ROOT $HOME/.pyenv
-fish_add_path $PYENV_ROOT/bin
-
-status is-login; and pyenv init --path | source
-status is-interactive; and pyenv init - | source
-
 # Created by `pipx` on 2021-12-16 10:15:00
 fish_add_path $HOME/.local/bin
 
