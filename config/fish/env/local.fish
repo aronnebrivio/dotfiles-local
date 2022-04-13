dfffd0dcc24286fd78e0ee8f74e31f1ad9196894
Aronne Brivio <aronne.brivio@gmail.com>
2022-04-13 12:32:00 +0200

    fix(fish): change homebrew packages paths in local env

--- config/fish/env/local.fish
+++ config/fish/env/local.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
-set -gx LDFLAGS "-L/opt/homebrew/Cellar/gettext/0.21/lib -L/opt/homebrew/Cellar/openssl@1.1/1.1.1m/lib/"
-set -gx CPPFLAGS "-I/opt/homebrew/Cellar/openssl@1.1/1.1.1m/include"
+set -gx LDFLAGS "-L/opt/homebrew/opt/gettext/lib -L/opt/homebrew/opt/openssl@1.1/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/openssl@1.1/include"
 # set -gx PATH /usr/local/homebrew/opt/libpq/bin $PATH
 
 # asdf
