24ca624be52bf60e8ac5cde251fc2126e33e18b5
Aronne Brivio <aronne.brivio@gmail.com>
2022-04-13 12:38:36 +0200

    chore(fish): add CA Labs CLI shell completions

--- config/fish/env/local.fish
+++ config/fish/env/local.fish
@@ -6,3 +6,6 @@ set -gx CPPFLAGS "-I/opt/homebrew/opt/openssl@1.1/include"
 
 # asdf
 source (brew --prefix asdf)/libexec/asdf.fish
+
+# CA Labs CLI
+complete --command cloudacademy-labs-cli --no-files --arguments "(env _CLOUDACADEMY_LABS_CLI_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) cloudacademy-labs-cli)" --condition "env _CLOUDACADEMY_LABS_CLI_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) cloudacademy-labs-cli"
