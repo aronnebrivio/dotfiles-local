cfb2af99b1ac4a2cef8150bbba4e397cf3008729
Aronne Brivio <aronne.brivio@gmail.com>
2023-06-09 12:57:26 +0200

    feat: add aptRoutine

--- config/fish/local.fish
+++ config/fish/local.fish
@@ -3,3 +3,11 @@
 function pyclean
     find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
 end
+
+# Upgrade and clean system with APT
+function aptRoutine
+	sudo apt update 2>/dev/null
+	sudo apt dist-upgrade
+	sudo apt autoremove -y
+	sudo apt autoclean
+end
