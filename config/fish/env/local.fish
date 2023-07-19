3c0cfa81089b017bf85280ef7d3eabf5d72f2b04
Aronne Brivio <aronne.brivio@cloudacademy.com>
2023-07-19 08:55:11 +0000

    feat: add updateOMP function, fix env and aptRoutine

--- config/fish/env/local.fish
+++ config/fish/env/local.fish
@@ -3,5 +3,6 @@
 source ~/.asdf/asdf.fish
 
 fish_add_path ~/.local/bin/
+fish_add_path ~/.cargo/bin/
 
 set -gx HOST_ENV aronne-brivio.remote-development
--- config/fish/local.fish
+++ config/fish/local.fish
@@ -7,7 +7,11 @@ end
 # Upgrade and clean system with APT
 function aptRoutine
 	sudo apt update 2>/dev/null
-	sudo apt dist-upgrade
+	sudo apt upgrade
 	sudo apt autoremove -y
 	sudo apt autoclean
+end
+
+function updateOMP
+	curl -s https://ohmyposh.dev/install.sh | sudo bash -s
 end
