e2d816ac0fb0ba583b26b13c10e36352defa6547
Aronne Brivio <aronne.brivio@cloudacademy.com>
2025-08-12 14:12:30 +0000

    fix(local): set HOST_ENV env variable

--- config/fish/env/local.fish
+++ config/fish/env/local.fish
@@ -5,4 +5,7 @@ source ~/.asdf/asdf.fish
 fish_add_path ~/.local/bin/
 fish_add_path ~/.cargo/bin/
 
-set -gx HOST_ENV aronne-brivio.remote-development
+set -gx HOST_ENV aronne-brivio.development
+#set -gx SHELL fish
+
+#. ~/.asdf/plugins/java/set-java-home.fish
